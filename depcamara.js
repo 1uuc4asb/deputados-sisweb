715c4b16b8aa95487662dd1c03cdea5e410ebf24
1uuc4asb <1uuc4asb@gmail.com>
2018-12-05 22:16:40 -0300

    Busca e resposta de deputados pelo nome já implementada.

--- depcamara.js
+++ depcamara.js
@@ -2,5 +2,13 @@ $(document).ready(function () {
 	console.log("Ready!");
 	$(document).on("click", "#busca-btn", function () {
 		alert("Clicou no botão!");
+		$.ajax({
+			method: "GET",
+			url: "https://dadosabertos.camara.leg.br/api/v2/deputados",
+			data: { nome: $("#deputado").val() }
+		}).done(function (msg) {
+			console.log(msg);
+			$("#deputados").html(msg);
+		})
 	});
 });
