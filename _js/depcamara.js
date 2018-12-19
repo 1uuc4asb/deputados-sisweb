73276c289608efdba65a95b68020bd8abc876a04
Danielflacerda <daniel.felacerda@hotmail.com>
2018-12-19 18:43:16 -0300

    Comecando a implementar visualizacao dos dados

--- _js/depcamara.js
+++ _js/depcamara.js
@@ -22,7 +22,7 @@ $(document).ready(function () {
 		}
 		else {
 			if(!$(this.parentNode).hasClass("has-content")) {
-				detalhesDeputado ($(this.parentNode).attr("id"));
+				detalhesDeputado ($(this.parentNode).attr("id"),$(this.parentNode).find(".details-div"));
 				$(this.parentNode).addClass("has-content");
 			}
 			$(this).css("transform", "rotate(180deg)");
@@ -49,7 +49,7 @@ $(document).ready(function () {
 				for(let i=0;i<result.length; i++) {
 					listahtml += "<li id=\"" + result[i].id + "\" class=\"deplist-item\"> <img class=\"depimg\" src=\"" + 
 					result[i].urlFoto + "\"/> <span class=\"resumo\"> Deputado: " + result[i].nome + " Partido: " + result[i].siglaPartido +
-					 " Estado: " + result[i].siglaUf + "</span> <input class=\"details\" type=\"image\" src=\"_imgs/arrow.png\" /> <br/> <div class=\"details-div\" style=\"box-shadow: 7px 7px 7px; margin: 1em; padding: 1em; display: none; background: white; border-radius: 5px;\"> Detalhes hu </div> </li>";
+					 " Estado: " + result[i].siglaUf + "</span> <input class=\"details\" type=\"image\" src=\"_imgs/arrow.png\" /> <br/> <div class=\"details-div\" style=\"box-shadow: 7px 7px 7px; margin: 1em; padding: 1em; display: none; background: white; border-radius: 5px;\"></div> </li>";
 				}
 				listahtml+= "</ul>";
 				$("#deputados").html(listahtml);
@@ -58,7 +58,7 @@ $(document).ready(function () {
 		});
 	}
 
-	function detalhesDeputado (id) {
+	function detalhesDeputado (id,detalhes) {
 		console.log("aa");
 		// Detalhes civis
 		$.ajax({
@@ -67,7 +67,49 @@ $(document).ready(function () {
 			dataType: "json"
 		}).done(function (msg) {
 			console.log("Detalhes" , msg);
+			let nome = msg.dados.nomeCivil;
+			let id_do_canditado = msg.dados.id;
+			let municipio = msg.dados.municipioNascimento;
+			let uf = msg.dados.ufNascimento;
+			let conteudo = "<h2> Dado civis do deputado </h2><div> Nome civil: " + nome + " <br/>Número de identificação do candidato: " + id_do_canditado +
+			"<br/>Estado de nascimento: " + uf + "<br/>Municipio de nascimento: " + municipio + "</div>";
+			detalhes.append(conteudo);
+			// Eventos
+
+			$.ajax({
+				method: "GET",
+				url: "https://dadosabertos.camara.leg.br/api/v2/deputados/" + id + "/eventos",
+				dataType: "json"
+			}).done(function (msg) {
+				console.log("Eventos: " , msg);
+				var conteudo = "<h2> Eventos que o(a) deputado(a) participou: </h2><div>";
+				for(let i=0; i<msg.dados.length; i++) {
+					var data,titulo,descricaoTipo,local,orgaos = "Não informado na base de dados";
+					
+					if(msg.dados[i].dataHoraInicio && msg.dados[i].dataHoraInicio != null) {
+						data = msg.dados[i].dataHoraInicio.substring(0,msg.dados[i].dataHoraInicio.indexOf("T"));
+					}
+					if(msg.dados[i].titulo && msg.dados[i].titulo != null) {
+						titulo = msg.dados[i].titulo;
+					}
+					if(msg.dados[i].descricaoTipo && msg.dados[i].descricaoTipo != null) {
+						descricaoTipo = msg.dados[i].descricaoTipo;
+					}
+					if(msg.dados[i].localCamara.nome && msg.dados[i].localCamara.nome != null) {
+						local = msg.dados[i].localCamara.nome;
+					}
+					var orgaos = "<div>Orgaos do evento:<br/>";
+					for(let j=0;j<msg.dados[i].orgaos.length;j++) {
+						orgaos += "Orgao: " + msg.dados[i].orgaos[j].nome + ", Tipo do orgao: " + msg.dados[i].orgaos[j].tipoOrgao + "<br/>";
+					}
+					orgaos += "</div>";
+					conteudo += "<div>Evento: " + titulo + "<br/>Tipo de evento: " + descricaoTipo + "<br/>Data do evento: " + data + "<br/>Local do evento: " + local + "<br/>" + orgaos + "<br/>";
+				}
+				detalhes.append(conteudo);
+			});
 		});
+
+		
 
 		// Despesas
 
@@ -77,27 +119,10 @@ $(document).ready(function () {
 			dataType: "json"
 		}).done(function (msg) {
 			console.log("Depesas: " , msg);
+			// Gerar grafico a partir das despesas
 		});
 
-		// Discursos
-
-		$.ajax({
-			method: "GET",
-			url: "https://dadosabertos.camara.leg.br/api/v2/deputados/" + id + "/discursos",
-			dataType: "json"
-		}).done(function (msg) {
-			console.log("Discursos: " , msg);
-		});
-
-		// Eventos
-
-		$.ajax({
-			method: "GET",
-			url: "https://dadosabertos.camara.leg.br/api/v2/deputados/" + id + "/eventos",
-			dataType: "json"
-		}).done(function (msg) {
-			console.log("Eventos: " , msg);
-		});
+		
 
 		// Orgaos
 
